b3626a274ab75134d92c9ac8c040d6607b7e143e
Avi Bagla <avibagla@gmail.com>
2015-02-19 16:01:43 -0800

    initial possible API

---
+++ IBMModel.py
@@ -0,0 +1,13 @@
+class IBM_Model_1:
+	'''Class which trains IBM Model 1 and allows for testing'''
+
+	def __init__(self):
+		pass
+
+	def train(self):
+		pass
+
+	def EMOneInstance(expectationList, primaryLanguage, secondaryLanguage):
+		pass
+
+	
