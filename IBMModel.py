fcc3b39e1773511c5c5d482fd19ec575d85a1544
Alex Sax <alexanderesax@gmail.com>
2015-02-21 19:15:36 -0800

    Implemented EM

--- IBMModel.py
+++ IBMModel.py
@@ -1,27 +1,25 @@
 from collections import defaultdict
+englishCorpusFile = './es-en/train/europarl-v7.es-en.en' #'../es-en/train/small.en' 
+spanishCorpusFile = './es-en/train/europarl-v7.es-en.es' # '../es-en/train/small.es'
+
+
 class IBM_Model_1:
 	"""Class which trains IBM Model 1 and allows for testing"""
 
 
 	def __init__(self):
-		self.englishCorpus = loadList('./es-en/train/europarl-v7.es-en.en')
-		self.spanishCorpus = loadList('./es-en/train/europarl-v7.es-en.es')
+		self.englishCorpus = loadList(englishCorpusFile)
+		self.spanishCorpus = loadList(spanishCorpusFile)
 		self.englishVocabulary = set()
 		self.spanishVocabulary = set()
+		self.null = "<<NULL>>"
 		for i, sentence in enumerate(self.englishCorpus):
 			self.englishVocabulary |= set(self.englishCorpus[i].split())
 			self.spanishVocabulary |= set(self.spanishCorpus[i].split())
+		self.englishVocabulary.add(self.null)
 
 
 	def train(self):
-		#alignmentProbabilities = Estep([{"green house", "casa verde"}, {"the house", "la casa"}], probabalityGrid)
-		# spanishCountDict = {}
-		#
-		# for word in self.spanishVocabulary:
-		# 	spanishCountDict[word] = countInv
-		# self.translate = {}
-		# for word in self.englishVocabulary:
-		# 	self.translate[word] = spanishCountDict.copy()
 		self.englishToIndex = {}
 		self.spanishToIndex = {}
 		countInv = 1./(len(self.spanishVocabulary)*len(self.englishVocabulary))
@@ -34,9 +32,11 @@ class IBM_Model_1:
 		for word in self.spanishVocabulary:
 			self.spanishToIndex[word] = i
 			i+=1
-
-
-
+		self.Estep(zip(self.englishCorpus, self.spanishCorpus))
+		self.Mstep()
+		# for f in self.spanishVocabulary:
+		# 	for e in self.englishVocabulary:
+		# 		print e,f,self.translate[self.englishToIndex[e]][self.spanishToIndex[f]]
 
 
 
@@ -44,18 +44,44 @@ class IBM_Model_1:
 
 		return Mstep(alignmentProbabilities, probabalityGrid)
 
-	def Estep(self, trainingPhrases, translationProbGrid):
+	def Estep(self, trainingPhrases):
 		"""Runs the Expectation Step of the IBM Model 1 algorithm"""
+		newTranslate = [[0]*len(self.spanishVocabulary) for i in range(len(self.englishVocabulary))]
 		for englishSentence, foreignSentence in trainingPhrases:
+			englishSentence = englishSentence.split()
+			englishSentence.append(self.null)
+			foreignSentence = foreignSentence.split()
 			#We first need to compute P(a, f |e) by multiplying all the t probabilities, following
-			pass
-		return alignmentProbabilities
+			translationProbability = self.findSentenceTranslationProb(englishSentence, foreignSentence)
+			for i in range(len(foreignSentence)):
+				for e in englishSentence:
+					f = foreignSentence[i]
+					newForeignSentence = list(foreignSentence)
+					newForeignSentence.pop(i)
+					transProbWithFixedEF = self.findSentenceTranslationProb(englishSentence, newForeignSentence)
+					t_ef = self.translate[self.englishToIndex[e]][self.spanishToIndex[f]] 
+					newTranslate[self.englishToIndex[e]][self.spanishToIndex[f]] += t_ef*transProbWithFixedEF/translationProbability
+					#print e,f, t_ef*transProbWithFixedEF/translationProbability
+		self.translate = newTranslate
+		#print self.translate
 
-	def Mstep(self, alignmentProbabilities, translationProbGrid):
+	def Mstep(self):
 		"""Runs the Maximization step of the IBM Model 1 algorithm"""
-		pass
+		for i in range(len(self.englishVocabulary)):
+			rowTotal = 0
+			for j in range(len(self.spanishVocabulary)):
+				rowTotal += self.translate[i][j]
+			for j in range(len(self.spanishVocabulary)):
+				self.translate[i][j]/=rowTotal
 
-
+	def findSentenceTranslationProb(self, englishSentence, foreignSentence):
+		normalizationFactor = 1.
+		for f in foreignSentence:
+			insideSum = 0
+			for e in englishSentence:
+				insideSum += self.translate[self.englishToIndex[e]][self.spanishToIndex[f]] 
+			normalizationFactor *= insideSum
+		return normalizationFactor
 
 def loadList(file_name):
     """Loads text files as lists of lines. Used in evaluation."""
@@ -70,5 +96,4 @@ def loadList(file_name):
 def main():
 	IBM_Model = IBM_Model_1()
 	IBM_Model.train() 
-
 main()
